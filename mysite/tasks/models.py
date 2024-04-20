e8a17fcf62749c3ace546293aa62a6f82e12734d
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-20 15:59:59 -0500

    modelo task

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class Task(models.Model):
+    name_text = models.CharField(max_length=100)
+    description_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
 # Create your models here.
