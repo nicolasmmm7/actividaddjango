438c85ddec1af94dd473c6dc8bd846925a598471
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-20 16:08:06 -0500

    atributo completo en el modelo

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -4,4 +4,5 @@ class Task(models.Model):
     name_text = models.CharField(max_length=100)
     description_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+    completed = models.BooleanField(default=False)
 # Create your models here.
