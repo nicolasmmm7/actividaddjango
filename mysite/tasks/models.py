90e9c9c962ff747d03ec35ec0ffd0e529f49f905
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-21 16:35:02 -0500

    import y nuevo metodo

--- mysite/tasks/models.py
+++ mysite/tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 class Task(models.Model):
     name_text = models.CharField(max_length=100)
     description_text = models.CharField(max_length=200)
@@ -8,4 +8,7 @@ class Task(models.Model):
     close_date = models.DateTimeField("date closed")
     def __str__(self):
         return self.name_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 # Create your models here.
