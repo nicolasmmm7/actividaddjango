5b58b8678e6a20320e3c92731c935e3edcebf24b
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-21 18:09:17 -0500

    views y urls

--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<int:task_id>/", views.descrip, name="descrip"),
+    path("<int:task_id>/completed/", views.complet, name="complet"),
 ]
--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello, world. You're at the tasks index.")
 # Create your views here.
+
+def descrip(request, task_id):
+    return HttpResponse("estas viendo la tarea %s." % task_id)
+
+def complet(request, task_id):
+    response = "estas viendo si la tarea fue completada %s."
+    return HttpResponse(response % task_id)
