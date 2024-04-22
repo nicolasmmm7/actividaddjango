e51093486219e3049ac37bf1fd48855f14db245c
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-21 19:30:15 -0500

    updates in views and templates

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Task
 from django.template import loader
-
+from django.http import Http404
 
 def index(request):
     latest_task_list = Task.objects.order_by("-pub_date")[:5]
@@ -14,7 +14,11 @@ def index(request):
 # Create your views here.
 
 def descrip(request, task_id):
-    return HttpResponse("estas viendo la tarea %s." % task_id)
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "tasks/descrip.html", {"task": task})
 
 def complet(request, task_id):
     response = "estas viendo si la tarea fue completada %s."
