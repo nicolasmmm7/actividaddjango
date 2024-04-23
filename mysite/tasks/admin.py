f3a99bf2add5cf1f9afab3b245409cdb63ff8386
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-23 08:34:34 -0500

    update admin.py

--- mysite/tasks/admin.py
+++ mysite/tasks/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Task
+from .models import User
+from .models import Category
+
 admin.site.register(Task)
+admin.site.register(User)
+admin.site.register(Category)
 
 # Register your models here.
