27b7179e3bb0fa58e7234546984fa739d474ca41
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-04-29 20:32:52 -0500

    implementaciones varias

--- mysite/tasks/views.py
+++ mysite/tasks/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Task
+from .models import Task, Category, User
 from django.template import loader
 from django.http import Http404
 
@@ -11,11 +11,22 @@ def index(request):
     }
     return render(request, "tasks/index.html", context)
 
+def p_category(request):
+    latest_category_list = Category.objects.all()
+    context = {
+        "latest_category_list": latest_category_list,
+    }
+    return render(request, "category/categorias.html", context)
+
 # Create your views here.
 
 def descrip(request, task_id):
         task = get_object_or_404(Task, pk=task_id)
         return render(request, "tasks/descrip.html", {"task": task})
+    
+def print_category(request, category_id):
+        category = get_object_or_404(Category, pk=category_id)
+        return render(request, "category/print_categorias.html", {"category": category})
     
 
 def complet(request, task_id):
