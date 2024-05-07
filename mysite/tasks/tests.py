252f6c5141bbed36febf051ad6512acdad26aaca
Nicolás Meneses <felipemenesespeinado5@gmail.com>
2024-05-06 20:00:16 -0500

    cambios varios

---
+++ mysite/tasks/tests.py
@@ -0,0 +1,36 @@
+import os
+import django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+django.setup()
+import json
+from django.urls import reverse
+from django.test import Client
+import pytest
+from .models import Task
+
+
+@pytest.mark.django_db
+def test_tasks_completed_list():
+    # Recuperamos todas las tareas de la base de datos
+    all_tasks = Task.objects.all()
+    completed_tasks = [task for task in all_tasks if task.completed]
+    print("Tareas completadas en la base de datos:", completed_tasks)
+
+    # Creamos un cliente de prueba
+    client = Client()
+
+    # Hacemos una solicitud a la vista tasks_completed_list
+    response = client.get(reverse("tasks:task_completed"))
+
+    # Verificamos que la solicitud haya sido exitosa (código de estado 200)
+    assert response.status_code == 200
+
+    # Convertimos el contenido de la respuesta en un diccionario JSON
+    task_data = json.loads(response.content)
+
+    # Verificamos que solo haya una tarea completada en la respuesta
+    assert len(task_data) == len([task for task in all_tasks if task.completed])
+
+    # Verificamos que la tarea en la respuesta sea la correcta
+    for task in task_data:
+        assert task["completed"] == True
--- mysite/tasks/urls.py
+++ mysite/tasks/urls.py
@@ -6,7 +6,7 @@ app_name = "tasks"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:task_id>/", views.descrip, name="descrip"),
-    path("task_completed/", views.tasks_completed_list, name="tasks_completeds"),
+    path("task_completed/", views.tasks_completed_list, name="task_completed"),
     path("category/", views.category_list, name= "category"),
     path("user/", views.user_list, name= "user")
 ]
